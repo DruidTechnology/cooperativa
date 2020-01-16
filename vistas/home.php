81b497ee93868a315c7dcc8f77324cf1de7048cd
Monty2107 <monterrosadelgado@gmail.com>
2020-01-16 11:32:56 -0600

    login finsh

--- vistas/home.php
+++ vistas/home.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-    <link rel="stylesheet" href="main.css">
-</head>
-<body>
-    <div id="menu">
-        <ul>
-            <li>Home</li>
-            <li class="cerrar-sesion"><a href="includes/logout.php">Cerrar sesión</a></li>
-        </ul>
-    </div>
+<?php
 
-    <section>
-        <h1>Bienvenido <?php echo $user->getNombre();  ?></h1>
-    </section>
-    
-</body>
-</html>
+@include("static/all/up.php");
+?>
+
+<?php
+
+@include("static/all/down.php");
+?>
